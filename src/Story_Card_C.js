0ca49220274759f9057946228c705cbe166732e7
dlashuel <dlashuel@gmail.com>
2020-09-07 20:04:33 -0700

    intial setup

---
+++ src/Story_Card_C.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Story_Card_C.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Card} from 'react-bootstrap';
+import {quran} from './quran.png'
+
+function Story_Card_C() {
+  return (
+    <div>
+        <h1>Hellooo</h1>
+        <image src="quran.png"  />
+    </div>
+  );
+}
+
+export default Story_Card_C;
