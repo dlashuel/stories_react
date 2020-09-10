49e82902dbc7f7cec7c050bb97ddaf50fa06681f
dlashuel <dlashuel@gmail.com>
2020-09-09 19:41:09 -0700

    header restyling

--- src/App.js
+++ src/App.js
@@ -8,11 +8,23 @@ import NavStory from './Nav.js';
 
 function App() {
   return (
-    <div className="App">  
+    <div className="App"> 
+        
         <NavStory />
+
         <Container fluid={true} id='header' className="mb-5">
+          <Row>
+            <Col lg={{offset:1, span:6}} className="text-left mt-5">
+              <h1 className='header-title'>"There's always room for a story <br></br>
+                that can transport people<br></br>
+                to another place."</h1>
+            </Col>
+            <Col>
+            </Col>
+          </Row>
           
         </Container>
+
         <Container fluid={true} >
           <Row>
             <Col xs={12} md={6} lg={4} className=''>
