aa2a00392088d3881d0f462c08d2fe2f78a8f8f6
dlashuel <dlashuel@gmail.com>
2020-09-09 23:47:31 -0700

    header buttons

--- src/App.js
+++ src/App.js
@@ -18,8 +18,11 @@ function App() {
               <h1 className='header-title'>"There's always room for a story <br></br>
                 that can transport people<br></br>
                 to another place."</h1>
+                <hr/>
             </Col>
-            <Col>
+            <Col className="row align-items-end">
+              <Button className="header-button mr-5" variant="outline-primary">Tell a Story</Button>
+              <Button className="header-button" variant="outline-primary">Read Stories</Button>
             </Col>
           </Row>
           
